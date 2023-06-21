a1b07990432a9c4468f207133a54ad90548bf843
Ammar <ammar.yassr.33@gmail.com>
2023-06-21 22:18:28 +0300

    Add CORS headers middleware

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+// Add CORS headers middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 // Parse JSON bodies
 app.use(express.json());
 
