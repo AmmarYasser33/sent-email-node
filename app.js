68f1cf1308f1750efab0d66dd9e9e8e6f868c5b7
Ammar <ammar.yassr.33@gmail.com>
2023-06-22 06:14:06 +0300

    add cors package

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 // Add CORS headers middleware
 app.use((req, res, next) => {
